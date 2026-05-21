a3e2d8e8a2335614081d1dd58cd34cf8f91c7a2d
Patricia-LF <patricia_lf_86@hotmail.com>
2026-05-21 18:25:51 +0200

    Added image to back to tivoli button

--- src/components/shared/BackToTivoliButton.tsx
+++ src/components/shared/BackToTivoliButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useGameStore } from "@/store/useGameStore";
+import Image from "next/image";
 
 const handleTivoliReturn = () => {
   useGameStore.getState().resetGame();
@@ -13,7 +14,13 @@ export function BackToTivoliButton() {
       onClick={handleTivoliReturn}
       className="z-50 text-sm text-white font-fell flex flex-col items-center cursor-pointer hover:opacity-80"
     >
-      <span className="text-6xl">🎪</span>
+      <Image
+        src="/assets/images/tivoli2.png"
+        alt=""
+        width={80}
+        height={80}
+        className="hover:scale-105 transition"
+      />
       Back to Tivoli
     </button>
   );
