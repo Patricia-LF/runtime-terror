22593cd90466a48a7252f0f448c7516eff0bbc71
Patricia-LF <patricia_lf_86@hotmail.com>
2026-05-23 20:07:51 +0200

    Updated size on button to be responsive

--- src/components/shared/BackToTivoliButton.tsx
+++ src/components/shared/BackToTivoliButton.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const handleTivoliReturn = () => {
   useGameStore.getState().resetGame();
-  window.parent.postMessage({ type: "AMUSEMENT_CLOSE" }, "")
+  window.parent.postMessage({ type: "AMUSEMENT_CLOSE" }, "");
   //window.location.href = process.env.NEXT_PUBLIC_TIVOLI_URL!;
 };
 
@@ -13,14 +13,14 @@ export function BackToTivoliButton() {
   return (
     <button
       onClick={handleTivoliReturn}
-      className="z-50 text-sm text-white font-fell flex flex-col items-center cursor-pointer hover:opacity-80"
+      className="z-50 text-sm text-white font-fell flex flex-col items-center justify-start cursor-pointer hover:opacity-80"
     >
       <Image
         src="/assets/images/tivoli2.png"
         alt=""
         width={80}
         height={80}
-        className="hover:scale-105 transition"
+        className="w-11 h-11 md:w-20 md:h-20 hover:scale-105 transition"
       />
       Back to Tivoli
     </button>
